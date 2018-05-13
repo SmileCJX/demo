cfa5d086c28816e5992640f6598376e772be06fe
SmileCJX <1092925421@qq.com>
2018-05-13 23:13:24 +0800

    datasource和sessionFactoryBean的配置

--- src/main/java/com/imooc/demo/Hello.java
+++ src/main/java/com/imooc/demo/Hello.java
@@ -1,4 +1,4 @@
-package com.imooc;
+package com.imooc.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- src/main/java/com/imooc/demo/entity/Area.java
+++ src/main/java/com/imooc/demo/entity/Area.java
@@ -1,4 +1,4 @@
-package com.imooc.entity;
+package com.imooc.demo.entity;
 
 import java.util.Date;
 
